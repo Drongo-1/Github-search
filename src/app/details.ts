2567d9126b7a9733dc43d7bf10b6a1d035dbd8f2
Drongo-1 <ngugidavid46@gmail.com>
2021-02-07 07:10:08 +0000

    added repository class

---
+++ src/app/details.ts
@@ -0,0 +1,12 @@
+export class Details {
+    constructor(public Name:string,
+    public url:any,
+    public avatar_url: any,
+    public public_repos:any,
+    public repo_url:any,
+    public bio:any,
+    public created_at:Date){
+
+    }
+
+}
